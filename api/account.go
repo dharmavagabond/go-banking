aed147e2455f1c5e7dd3030a9cfa9ab2c40ac3af
Dharma <omar.figueroa@protonmail.com>
2022-11-16 22:26:30 -0600

    feat: agregué la creación y listado de las cuentas

---
+++ api/account.go
@@ -0,0 +1,103 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/dharmavagabond/simple-bank/db/sqlc"
+	"github.com/jackc/pgx/v4"
+	"github.com/labstack/echo/v4"
+)
+
+type (
+	createAccountRequest struct {
+		Owner    string `json:"owner" validate:"required"`
+		Currency string `json:"currency" validate:"required,oneof=USD EUR"`
+	}
+
+	getAccountRequest struct {
+		ID int64 `param:"id" validate:"required,min=1"`
+	}
+
+	listAccountRequest struct {
+		PageID   int32 `query:"page_id" validate:"required,min=1"`
+		PageSize int32 `query:"page_size" validate:"required,min=5,max=10"`
+	}
+)
+
+func (server *Server) createAccount(ectx echo.Context) (err error) {
+	var account db.Account
+	req := &createAccountRequest{}
+
+	if err = ectx.Bind(req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if err = ectx.Validate(req); err != nil {
+		return err
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+	}
+
+	if account, err = server.store.CreateAccount(ectx.Request().Context(), arg); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return ectx.JSON(http.StatusOK, account)
+}
+
+func (server *Server) getAccount(ectx echo.Context) (err error) {
+	var account db.Account
+	req := &getAccountRequest{}
+
+	if err = ectx.Bind(req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if err = ectx.Validate(req); err != nil {
+		return err
+	}
+
+	if account, err = server.store.GetAccount(ectx.Request().Context(), req.ID); err != nil {
+		if err == pgx.ErrNoRows {
+			return echo.NewHTTPError(http.StatusNotFound, err.Error())
+		}
+
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return ectx.JSON(http.StatusOK, account)
+}
+
+func (server *Server) listAccounts(ectx echo.Context) (err error) {
+	var accounts []db.Account
+	req := &listAccountRequest{
+		PageID:   1,
+		PageSize: 5,
+	}
+
+	if err = ectx.Bind(req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if err = ectx.Validate(req); err != nil {
+		return err
+	}
+
+	arg := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	if accounts, err = server.store.ListAccounts(ectx.Request().Context(), arg); err != nil {
+		if err == pgx.ErrNoRows {
+			return echo.NewHTTPError(http.StatusNotFound, err.Error())
+		}
+
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return ectx.JSON(http.StatusOK, accounts)
+}
--- go.mod
+++ go.mod
@@ -3,14 +3,21 @@ module github.com/dharmavagabond/simple-bank
 go 1.19
 
 require (
+	github.com/MadAppGang/httplog v1.2.1
 	github.com/Pallinder/go-randomdata v1.2.0
+	github.com/go-playground/validator/v10 v10.11.1
 	github.com/jackc/pgconn v1.13.0
 	github.com/jackc/pgx/v4 v4.17.2
+	github.com/labstack/echo/v4 v4.9.1
 	github.com/stretchr/testify v1.8.1
 )
 
 require (
+	github.com/TylerBrock/colorjson v0.0.0-20200706003622-8a50f05110d2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -18,8 +25,17 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.12.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
