bc84a1a1896c058521bd3de9ec676bedc035dc2c
Dharma <omar.figueroa@protonmail.com>
2023-09-25 12:50:00 -0600

    refactor(util): eliminé errores del lint
    Signed-off-by: Dharma <omar.figueroa@protonmail.com>

--- internal/util/random.go
+++ internal/util/random.go
@@ -1,16 +1,9 @@
 package util
 
-import (
-	"math/rand"
-	"time"
-)
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
+import "math/rand"
 
 func RandomInt(min, max int64) int64 {
-	return min + rand.Int63n(max-min+1)
+	return min + rand.Int63n(max-min+1) //nolint: gosec // No se genera un token
 }
 
 func RandomMoney() int64 {
