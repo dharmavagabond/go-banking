1b7d24a8f2658a476005b1e4bbe8e980235d5dd1
Dharma <omar.figueroa@protonmail.com>
2023-11-07 21:34:33 -0600

    fix(conf): valido que el puerto http no sea negativo

--- internal/config/app.go
+++ internal/config/app.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"time"
 
 	"github.com/cristalhq/aconfig"
@@ -12,7 +13,7 @@ type AppConfig = struct {
 	Env                  string        `default:"production" env:"ENV"`
 	Secret               string        `                     env:"SECRET"`
 	TokenSymmetricKey    string        `                     env:"TOKEN_SYMMETRIC_KEY"`
-	HTTPPort             int           `default:"8080"       env:"HTTP_PORT"`
+	HTTPPort             int           `default:"0"          env:"HTTP_PORT"`
 	GrpcPort             int           `default:"9090"       env:"GRPC_PORT"`
 	AccessTokenDuration  time.Duration `default:"15m"        env:"ACCESS_TOKEN_DURATION"`
 	RefreshTokenDuration time.Duration `default:"24h"        env:"REFRESH_TOKEN_DURATION"`
@@ -28,6 +29,10 @@ func init() {
 
 	if err := loader.Load(); err != nil {
 		log.Fatal().Err(err)
+	}
+
+	if App.HTTPPort < 0 {
+		log.Fatal().Err(errors.New("el puerto no puede ser negativo"))
 	}
 
 	App.IsDev = App.Env == "development"
